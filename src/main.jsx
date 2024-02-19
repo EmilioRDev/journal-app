1feebb9e9beb082d90ec42eedacf8403e7909f87
EmilioRDev <joseemilio0501@gmail.com>
2024-02-19 20:25:45 +0100

    Configuración Redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles.css';
-import { JournalApp } from './JournalApp';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { store } from './store';
+import './styles.css';
 
+import { JournalApp } from './JournalApp';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<BrowserRouter>
-		<React.StrictMode>
-			<JournalApp />
-		</React.StrictMode>
-	</BrowserRouter>
+	<React.StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+				<JournalApp />
+			</BrowserRouter>
+		</Provider>
+	</React.StrictMode>
 );
